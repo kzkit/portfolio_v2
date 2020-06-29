468633eb2a8c33f1be176f7f36289c5266997294
Zhen Kit <zhenkit1996@gmail.com>
2020-06-29 16:55:51 +0800

    added css for 1920w resolution

--- components/about.vue
+++ components/about.vue
@@ -1,25 +1,27 @@
 <template>
   <article>
     <div class="container mx-auto px-4">
-      <div class="xl:flex xl:justify-between xl:flex-row-reverse">
+      <div
+        class="xl:flex xxl:flex xxl:justify-between xl:justify-between xl:flex-row-reverse xxl:flex-row-reverse"
+      >
         <div>
           <p
-            class="font-sans font-extrabold text-3xl xl:text-6xl text-gray-800 text-center xl:mt-20 xl:text-right xl:mr-16"
+            class="font-sans font-extrabold text-3xl xl:text-6xl xxl:text-6xl text-gray-800 text-center xl:mt-20 xxl:mt-20 xl:text-right xxl:text-right xxl:mr-16 xl:mr-16"
           >
             <span
               class="half_background dark:text-gray-300 transition duration-500 ease-in-out"
             >Hello</span>
           </p>
         </div>
-        <div class="xl:mr-64 xl:pr-64 mt-6 xl:ml-12">
+        <div class="xl:mr-64 xxl:mr-64 xxl:pr-64 xxl:ml-12 xl:pr-64 mt-6 xl:ml-12">
           <p
-            class="font-sans font-bold text-2xl xl:text-3xl text-gray-800 text-center xl:text-left leading-relaxed dark:text-gray-300 transition duration-500 ease-in-out"
+            class="font-sans font-bold text-2xl xl:text-3xl xxl:text-3xl text-gray-800 text-center xl:text-left xxl:text-left leading-relaxed dark:text-gray-300 transition duration-500 ease-in-out"
           >Hi, I'm Kit! 🙋🏻‍♂️</p>
           <p
-            class="font-sans font-medium text-xl xl:text-2xl text-gray-800 text-center xl:text-left leading-relaxed dark:text-gray-300 transition duration-500 ease-in-out"
+            class="font-sans font-medium text-xl xl:text-2xl xxl:text-2xl xxl:text-left text-gray-800 text-center xl:text-left leading-relaxed dark:text-gray-300 transition duration-500 ease-in-out"
           >I am a fullstack developer based in Kuala Lumpur, MY.</p>
           <p
-            class="font-sans font-medium text-xl xl:text-2xl text-gray-800 text-center xl:text-left leading-relaxed dark:text-gray-300 transition duration-500 ease-in-out"
+            class="font-sans font-medium text-xl xl:text-2xl xxl:text-2xl xxl:text-left text-gray-800 text-center xl:text-left leading-relaxed dark:text-gray-300 transition duration-500 ease-in-out"
           >I enjoy tinkering with new technologies and leverage it to solve problems and build amazing applications!</p>
         </div>
       </div>
--- components/skill.vue
+++ components/skill.vue
@@ -1,36 +1,36 @@
 <template>
   <article>
     <div class="container mx-auto px-4 mt-12">
-      <div class="xl:flex xl:justify-between">
-        <div class="my-6 xl:my-64 xl:ml-12">
+      <div class="xl:flex xl:justify-between xxl:flex xxl:justify-between">
+        <div class="my-6 xl:my-64 xl:ml-12 xxl:my-64 xxl:ml-12">
           <p
-            class="font-sans font-extrabold text-3xl xl:text-6xl text-gray-800 text-center xl:text-right dark:text-gray-300 transition duration-500 ease-in-out"
+            class="font-sans font-extrabold text-3xl xl:text-6xl xxl:text-6xl xxl:text-right text-gray-800 text-center xl:text-right dark:text-gray-300 transition duration-500 ease-in-out"
           >
             <span class="half_background">Skills</span>
           </p>
         </div>
-        <div class="my-12 mt-6 xl:my-64 xl:mr-16">
+        <div class="my-12 mt-6 xl:my-64 xl:mr-16 xxl:my-64 xxl:mr-16">
           <p
-            class="font-sans font-bold text-2xl xl:text-2xl text-gray-800 text-center xl:text-right dark:text-gray-300 transition duration-500 ease-in-out"
+            class="font-sans font-bold text-2xl xl:text-2xl xxl:text-3xl xxl:text-right text-gray-800 text-center xl:text-right dark:text-gray-300 transition duration-500 ease-in-out"
           >Here are a few technologies I've been working with recently 🚀</p>
           <ul class="list-none grid grid-cols-2 gap-6 mt-6">
             <li
-              class="font-sans font-medium text-xl xl:text-xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
+              class="font-sans font-medium text-xl xl:text-xl xxl:text-2xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
             >Flutter</li>
             <li
-              class="font-sans font-medium text-xl xl:text-xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
+              class="font-sans font-medium text-xl xl:text-xl xxl:text-2xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
             >Vue</li>
             <li
-              class="font-sans font-medium text-xl xl:text-xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
+              class="font-sans font-medium text-xl xl:text-xl xxl:text-2xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
             >JavaScript (ES6+)</li>
             <li
-              class="font-sans font-medium text-xl xl:text-xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
+              class="font-sans font-medium text-xl xl:text-xl xxl:text-2xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
             >NodeJS</li>
             <li
-              class="font-sans font-medium text-xl xl:text-xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
+              class="font-sans font-medium text-xl xl:text-xl xxl:text-2xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
             >MongoDB</li>
             <li
-              class="font-sans font-medium text-xl xl:text-xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
+              class="font-sans font-medium text-xl xl:text-xl xxl:text-2xl text-gray-800 text-center dark:text-gray-300 transition duration-500 ease-in-out"
             >SQL</li>
           </ul>
         </div>
