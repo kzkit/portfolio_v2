c36ca00bfaebe7f717764633e82b3d697f0f06fa
Zhen Kit <zhenkit1996@gmail.com>
2020-06-29 15:22:16 +0800

    fix mbp css issue

--- components/header.vue
+++ components/header.vue
@@ -2,25 +2,25 @@
   <header>
     <div class="container mx-auto h-screen">
       <div class="xl:flex xl:justify-between xl:flex-row-reverse">
-        <div class="xl:flex-1 xl:relative mt-32">
+        <div class="xl:flex-1 mt-32">
           <img
-            class="xl:w-full xl:object-right xl:absolute xl:ml-64"
+            class="xl:w-9/12 xl:ml-48"
             src="../assets/img/gummy-programming.png"
             alt="programming"
           />
         </div>
-        <div class="xl:flex-3 xl:mt-64 text-center xl:text-left">
+        <div class="xl:flex-3 xl:mt-64 xl:ml-12 text-center xl:text-left">
           <p
             class="font-sans font-extrabold text-3xl xl:text-6xl text-gray-800 dark:text-gray-300 transition duration-500 ease-in-out"
           >
             <span class="half_background">Kong Zhen Kit</span>
           </p>
           <p
-            class="font-sans text-xl xl:text-4xl font-bold text-gray-800 dark:text-gray-300 transition duration-500 ease-in-out"
+            class="font-sans text-xl xl:text-3xl font-bold text-gray-800 dark:text-gray-300 transition duration-500 ease-in-out"
           >Fullstack Developer</p>
           <div class="flex justify-center xl:justify-start">
             <a class="my-2 mx-2" rel="noreferrer" href="https://github.com/kzkit" target="_blank">
-              <img class="w-5 lg:w-8 xl:w-10" src="../assets/img/github.svg" alt="github" />
+              <img class="w-5 xl:w-10" src="../assets/img/github.svg" alt="github" />
             </a>
             <a
               class="my-2 mx-2"
