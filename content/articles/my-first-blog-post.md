67564ab2396d02a298705533a626459f839714a6
Zhen Kit <zhenkit1996@gmail.com>
2020-07-06 01:04:22 +0800

    added blog css

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -3,8 +3,18 @@ title: My first Blog Post
 description: Learning how to use @nuxt/content to create a blog
 img: first-blog-post.jpg
 alt: my first blog post
+author:
+  name: Zhen Kit
+  bio: Mobile Developer and Advocate
 ---
 
 # Hello world!
 
-Welcome to my first blog post using content module
+Zi is the best and super pretty <3
+
+```js
+var x, y, z; // Declare 3 variables
+x = 5; // Assign the value 5 to x
+y = 6; // Assign the value 6 to y
+z = x + y; // Assign the sum of x and y to z
+```
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
+  <div
+    class="overflow-hidden dark:bg-blue-900 transition duration-500 ease-in-out"
+  >
     <Nav />
     <div class="container mx-auto h-screen">
       <article>
-        <h1>{{ article.title }}</h1>
-        <nuxt-content :document="article" />
+        <h1
+          class="pt-12 text-center font-sans font-extrabold text-3xl xl:text-6xl xxl:text-6xl text-gray-800 dark:text-gray-300 transition duration-500 ease-in-out"
+        >
+          {{ article.title }}
+        </h1>
+        <h2
+          class="text-center text-gray-800 dark:text-gray-300 transition duration-500 ease-in-out"
+        >
+          {{ formatDate(article.createdAt) }}
+        </h2>
+        <h2
+          class="text-center text-gray-800 dark:text-gray-300 transition duration-500 ease-in-out"
+        >
+          Written by {{ article.author.name }}
+        </h2>
+        <nuxt-content
+          class="container mx-auto mt-12 px-6 dark:text-gray-300 transition duration-500 ease-in-out"
+          :document="article"
+        />
       </article>
     </div>
     <Footer />
@@ -38,7 +57,7 @@ export default {
         {
           hid: "description",
           name: "description",
-          content: "Article"
+          content: "article by zhen kit"
         }
       ]
     };
@@ -46,4 +65,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.nuxt-content h1 {
+  @apply text-3xl font-bold font-sans;
+}
+
+.nuxt-content h2 {
+  @apply text-2xl font-bold font-sans;
+}
+
+.nuxt-content h3 {
+  @apply text-xl font-bold font-sans;
+}
+
+.nuxt-content p {
+  font-size: 1.125rem;
+}
+</style>
